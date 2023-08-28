08fa495bb939bbbc87774c8baab27ed50f94b59e
Johnatan Ortiz <6403@holbertonstudents.com>
2023-08-28 10:20:57 -0500

    concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''
+-------------------------------------------------------------------------------
+MODULE NAME : 0-add
+-------------------------------------------------------------------------------
+MODULE DESCRIPTION:
+-------------------------------------------------------------------------------
+    Write a type-annotated function concat that takes a string str1 and a strin
+    g str2 as arguments and returns a concatenated string
+-------------------------------------------------------------------------------
+
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    """ Return a concatenaded string """
+
+    return str1 + str2
