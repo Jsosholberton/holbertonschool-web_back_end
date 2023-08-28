c1dba40263fe6220f5e0ab77009850c0f9ddb46a
Johnatan Ortiz <6403@holbertonstudents.com>
2023-08-28 10:08:46 -0500

    first point

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+'''
+-------------------------------------------------------------------------------
+MODULE NAME : 0-add
+-------------------------------------------------------------------------------
+MODULE DESCRIPTION:
+-------------------------------------------------------------------------------
+    Write a type-annotated function add that takes a float a and a float b as a
+    rguments and returns their sum as a float.
+-------------------------------------------------------------------------------
+
+'''
+
+def add(a:float, b:float) -> float:
+    """Return the sum of a 2 floats"""
+    return a + b
