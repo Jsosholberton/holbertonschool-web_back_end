97b8ba899b175cc6ab0a194881312025e04291fb
Johnatan Ortiz <6403@holbertonstudents.com>
2023-08-28 11:02:26 -0500

    sum mixed list

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 '''
 -------------------------------------------------------------------------------
-MODULE NAME : 5-sum_list
+MODULE NAME : 6-sum_mixed_list
 -------------------------------------------------------------------------------
 MODULE DESCRIPTION:
 -------------------------------------------------------------------------------
-    Write a type-annotated function sum_list which takes a list input_list of f
-    loats as argument and returns their sum as a float.
+    Write a type-annotated function sum_mixed_list which takes a list mxd_lst o
+    f integers and floats and returns their sum as a float.
 -------------------------------------------------------------------------------
 '''
-from typing import List
+from typing import List, Union
 
 
-def sum_list(input_list: List[float]) -> float:
-    """return the sum of a list of floats"""
-    return sum(input_list)
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """return the sum of a list"""
+    return float(sum(mxd_lst))
