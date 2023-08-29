9e22894ca11c3e51875f41b6a5d8ac66f566a53a
Johnatan Ortiz <6403@holbertonstudents.com>
2023-08-29 12:07:53 -0500

    tasks 4

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,34 +1,27 @@
 #!/usr/bin/env python3
 '''
 -------------------------------------------------------------------------------
-MODULE NAME : 1-concurrent_coroutines
+MODULE NAME : 4-task
 -------------------------------------------------------------------------------
 MODULE DESCRIPTION:
 -------------------------------------------------------------------------------
-    Import wait_random from the previous python file that you've written and wr
-    ite an async routine called wait_n that takes in 2 int arguments (in this o
-    rder): n and max_delay. You will spawn wait_random n times with the specifi
-    ed max_delay.
-
-    wait_n should return the list of all the delays (float values). The list of
-    the delays should be in ascending order without using sort() because of con
-    currency.
+    Take the code from wait_n and alter it into a new function task_wait_n. The
+    code is nearly identical to wait_n except task_wait_random is being called.
 -------------------------------------------------------------------------------
 IMPORTS:
 -------------------------------------------------------------------------------
     asyncio: to use the Coroutine that completes after a given time(in seconds)
-    wait_random: that funcion return a random time sync
 -------------------------------------------------------------------------------
 '''
 import asyncio
 from typing import List
 
+
 get = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    ''' Function that returns a list
-    '''
-    l = [get(max_delay) for i in range(n)]
-    finish = [await task for task in asyncio.as_completed(l)]
+    """return a list of task"""
+    list_task = [get(max_delay) for i in range(n)]
+    finish = [await task for task in asyncio.as_completed(list_task)]
     return finish
