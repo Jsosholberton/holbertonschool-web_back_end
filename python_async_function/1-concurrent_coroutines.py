19ae23d2587f7a928ceea97ff57014a8eb8921fb
Johnatan Ortiz <6403@holbertonstudents.com>
2023-08-29 10:37:34 -0500

    fixed sorted

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -30,4 +30,5 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Return a list of dalays"""
     list_delay = [await wait_random(max_delay) for _ in range(n)]
+    list_delay.sort()
     return list_delay
