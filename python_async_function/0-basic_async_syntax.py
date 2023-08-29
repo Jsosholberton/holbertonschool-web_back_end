a1278c45ee5034870c5478ce2d022e082492806c
Johnatan Ortiz <6403@holbertonstudents.com>
2023-08-29 10:29:47 -0500

    fixed documents

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -21,6 +21,7 @@ import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
+    """Return the delay"""
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -28,5 +28,6 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Return a list of dalays"""
     list_delay = [await wait_random(max_delay) for _ in range(n)]
     return list_delay
