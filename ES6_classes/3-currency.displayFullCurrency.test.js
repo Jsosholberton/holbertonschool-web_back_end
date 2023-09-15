313647a967a3dc221815b50b27e3c82ce1748846
Johnatan Ortiz <6403@holbertonstudents.com>
2023-09-15 17:48:26 -0500

    fix

---
+++ ES6_classes/3-currency.displayFullCurrency.test.js
@@ -0,0 +1,8 @@
+import Currency from "./3-currency.js";
+
+const dollar = new Currency('$', 'Dollars');
+
+test("displayFullCurrency is implemented correctly", () => {
+    expect(dollar.displayFullCurrency()).toBe('Dollars ($)');
+});
+ 
