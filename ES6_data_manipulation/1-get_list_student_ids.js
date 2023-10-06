d4d14e27a79f358a410fde5ec3aa52783f02bbe9
Johnatan Ortiz <6403@holbertonstudents.com>
2023-10-06 10:13:27 -0500

    fix

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,5 +1,5 @@
 export default function getListStudentIds(datas) {
-  if (typeof datas === 'object') {
+  if (typeof datas === 'object' && datas !== null) {
     return datas.map((data) => data.id);
   }
   return [];
