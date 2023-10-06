221cc48c0090ce3111d9cc1f1d831ae0418e0f55
Johnatan Ortiz <6403@holbertonstudents.com>
2023-10-06 11:54:27 -0500

    slint fall

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,4 @@
+
 export default function getStudentsByLocation(datas, city) {
   if (datas && city && typeof datas === 'object') {
     return datas.filter((data) => data.location === city);
---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+import getStudentsByLocation from './2-get_students_by_loc';
+
+export default function updateStudentGradeByCity(datas, city, newGrades) {
+  const filterByCity = getStudentsByLocation(datas, city);
+  return filterByCity.map((student) => ({
+    ...student,
+    grade:
+      newGrades.filter((gradeInfo) => gradeInfo.studentId === student.id)[0]?.grade || "N/A",
+  }));
+}
+/* eslint-enable */
